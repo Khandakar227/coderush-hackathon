4e3c29e6602867843a6bdb2678dce7f332eb9eb9
Khandakar Shakib <xenonknight54@gmail.com>
2023-03-18 09:01:49 +0600

    Structured

--- application/src/pages/index.tsx
+++ application/src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <>
       <Head>
         <title>Next gen CV</title>
-        <meta name="description" content="An automated CV generator web app" />
+        <meta name="description" content="An automated CV generator web app+" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
