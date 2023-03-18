9aa5bc8f9fd3ea2f7aa16598c49a7739ece8d988
Khandakar Shakib <xenonknight54@gmail.com>
2023-03-18 18:55:48 +0600

    cv-generator almost done

--- application/src/pages/cv-generator.tsx
+++ application/src/pages/cv-generator.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState , useEffect} from "react";
 import Input from "@/components/cv-generator/Input";
+import { CVDataStorageName } from "@/config";
 
 interface ClientDataProps {
   about: string[];
@@ -13,29 +14,40 @@ interface ClientDataProps {
   profession: string[];
   skills: string[];
   username: string[];
-  [key:string]: any;
+  [key: string]: any;
 }
 
 function CVgenerator() {
   const [clientData, setClientData] = useState<ClientDataProps>();
 
+  useEffect(() => {
+    const data  = localStorage.getItem(CVDataStorageName);
+    try {
+        const parsedData = JSON.parse(data as string);
+        setClientData(parsedData);
+    } catch (error) {
+        console.log("Error occured", error);
+    }
+  },[])
+
   const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget as HTMLFormElement);
-    
+
     const data = {} as ClientDataProps;
-    
+
     for (const [name, value] of formData.entries()) {
-        // If the field name already exists in the data object, push the new value to the existing array
-        if (data[name]) {
-          data[name].push(value);
-        } else {
-          // If the field name doesn't exist in the data object, create a new array with the first value
-          data[name] = [value];
-        }
+      // If the field name already exists in the data object, push the new value to the existing array
+      if (data[name]) {
+        data[name].push(value);
+      } else {
+        // If the field name doesn't exist in the data object, create a new array with the first value
+        data[name] = [value];
       }
+    }
+    localStorage.setItem(CVDataStorageName, JSON.stringify(data));
     console.log(data);
-    
+
     setClientData(data);
   };
   return (
@@ -54,7 +66,6 @@ function CVgenerator() {
         <form className="glass-morph p-4" onSubmit={handleFormSubmit}>
           <h3 className="text-xl p-4 font-bold"> Display pictue </h3>
           <input
-          
             className="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 dark:border-neutral-600 bg-clip-padding py-[0.32rem] px-3 text-base font-normal text-neutral-700 dark:text-neutral-200 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-black dark:file:bg-neutral-700 file:px-3 file:py-[0.32rem] file:text-white dark:file:text-neutral-100 file:transition file:duration-150 file:ease-in-out file:[margin-inline-end:0.75rem] file:[border-inline-end-width:1px] hover:file:text-black hover:file:bg-neutral-200 focus:border-primary focus:text-black focus:shadow-[0_0_0_1px] focus:shadow-primary focus:outline-none"
             id="display_picture"
             type="file"
@@ -65,6 +76,7 @@ function CVgenerator() {
             label="Name"
             name="username"
             type="text"
+
             placeholder="Enter your name"
           />
           <Input
@@ -114,10 +126,7 @@ function CVgenerator() {
           />
 
           <hr className="my-4" />
-          <h3 className="text-xl p-4 font-bold">
-            
-            Educational Qualifications
-          </h3>
+          <h3 className="text-xl p-4 font-bold">Educational Qualifications</h3>
           <Input
             element="textarea"
             label="Tell us about your educational background"
@@ -156,13 +165,12 @@ function CVgenerator() {
             className="my-4 py-4 px-8 shadow rounded-md bg-black text-white"
             type="submit"
           >
-            
             Submit
           </button>
         </form>
       </div>
       <div className="lg:fixed top-[68px] custom-h md:max-w-2xl w-full md:right-[10px] p-4">
-        <div className="bg-white text-sm w-full h-full border shadow">
+        <div className="bg-white text-sm w-full h-full border shadow overflow-auto">
           <div className="bg-blue-600 text-white px-4 py-8 flex gap-4 justify-start items-center">
             <img
               src="/assets/dp_temp.png"
@@ -186,13 +194,20 @@ function CVgenerator() {
 
                 <tr className="py-4 border-y-2">
                   <td className="field"> Date of Birth </td>
-                  <td className="field-value"> {clientData?.date_of_birth[0]} </td>
+                  <td className="field-value">
+                    {" "}
+                    {clientData?.date_of_birth[0]}{" "}
+                  </td>
                 </tr>
 
                 <tr className="py-4 border-y-2">
                   <td className="field"> Adddress </td>
-                  <td className="field-value"> {clientData?.city[0]} {clientData?.city.length ?",":""} {clientData?.country[0]} </td>
-                </tr> 
+                  <td className="field-value">
+                    {" "}
+                    {clientData?.city[0]} {clientData?.city.length ? "," : ""}{" "}
+                    {clientData?.country[0]}{" "}
+                  </td>
+                </tr>
               </tbody>
             </table>
           </div>
@@ -222,7 +237,10 @@ function CVgenerator() {
           </div>
 
           <div className="m-4 p-4 border rounded-md bg-gray-100">
-            <h4 className="text-sm text-blue-600 font-bold py-1"> Educational Qualifications </h4>
+            <h4 className="text-sm text-blue-600 font-bold py-1">
+              {" "}
+              Educational Qualifications{" "}
+            </h4>
             <hr />
             <p className="whitespace-pre-wrap">{clientData?.education[0]}</p>
           </div>
@@ -230,7 +248,12 @@ function CVgenerator() {
           <div className="m-4 p-4 border rounded-md bg-gray-100">
             <h4 className="text-sm text-blue-600 font-bold py-1"> Skillset </h4>
             <hr />
-            {clientData?.skills.map((skill,i) => <p key={`${i}. ${skill}`} className="py-1"> {i+1}.{skill}</p>)}
+            {clientData?.skills.map((skill, i) => (
+              <p key={`${i}. ${skill}`} className="py-1">
+                {" "}
+                {i + 1}.{skill}
+              </p>
+            ))}
           </div>
         </div>
       </div>
